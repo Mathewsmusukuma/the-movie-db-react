301475d90edcf79388bb524c9d57f8c686110b9a
sikaili99 <mathewsmusukuma@outlook.com>
2019-05-08 14:13:54 +0200

    Updates

---
+++ precache-manifest.5fc808a4b8eb274782d1fdba93b38618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3257dd420a9a5b673cccac692d8637f1",
+    "url": "/the-movie-db-react/index.html"
+  },
+  {
+    "revision": "3100892f86b217243205",
+    "url": "/the-movie-db-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "cb2ffb3b1b6625d74d51",
+    "url": "/the-movie-db-react/static/css/main.822bc24d.chunk.css"
+  },
+  {
+    "revision": "3100892f86b217243205",
+    "url": "/the-movie-db-react/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "cb2ffb3b1b6625d74d51",
+    "url": "/the-movie-db-react/static/js/main.4abc6169.chunk.js"
+  },
+  {
+    "revision": "1a52748fbec1885f37a3",
+    "url": "/the-movie-db-react/static/js/runtime~main.c2eb818a.js"
+  }
+]);
